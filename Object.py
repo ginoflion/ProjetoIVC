dcb2a63254b891fd4b864d11e4a64970fb225b64
Goncalo Silva <goncalosilva120899@gmail.com>
2023-11-12 02:16:44 +0000

    done 2

--- Object.py
+++ Object.py
@@ -15,8 +15,6 @@ class YOLOIntegration:
 
         cv2.namedWindow("Image")
 
-
-
     def process_frame(self, image):
         image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2RGB)
         results = self.model(image, verbose=False)
@@ -30,7 +28,34 @@ class YOLOIntegration:
             confidence = box[4]
             class_id = int(box[5])
             if class_id == 67 and confidence > 0.25:
-                self.draw_object_indicator(image_objects, box)
+                cx = int((box[0] + box[2]) / 2)
+                cy = int((box[1] + box[3]) / 2)
+
+                # Draw rectangles based on phone's position
+                if cx > (2 / 3) * image_objects.shape[1] and (1 / 3) * image_objects.shape[0] < cy < (2 / 3) * \
+                        image_objects.shape[0]:
+                    cv2.rectangle(img=image_objects, pt1=(image_objects.shape[1] - 10, 0),
+                                  pt2=(image_objects.shape[1], image_objects.shape[0]),
+                                  color=(255, 255, 255), thickness=6)
+                elif cx < (1 / 3) * image_objects.shape[1] and (1 / 3) * image_objects.shape[0] < cy < (2 / 3) * \
+                        image_objects.shape[0]:
+                    cv2.rectangle(img=image_objects, pt1=(0, 0), pt2=(10, image_objects.shape[0]),
+                                  color=(255, 255, 255), thickness=6)
+                elif cy > (2 / 3) * image_objects.shape[0] and (1 / 3) * image_objects.shape[1] < cx < (2 / 3) * \
+                        image_objects.shape[1]:
+                    cv2.rectangle(img=image_objects, pt1=(0, image_objects.shape[0] - 10),
+                                  pt2=(image_objects.shape[1], image_objects.shape[0]),
+                                  color=(255, 255, 255), thickness=6)
+                elif cy < (1 / 3) * image_objects.shape[0] and (1 / 3) * image_objects.shape[1] < cx < (2 / 3) * \
+                        image_objects.shape[1]:
+                    cv2.rectangle(img=image_objects, pt1=(0, 0), pt2=(image_objects.shape[1], 10),
+                                  color=(255, 255, 255), thickness=6)
+                self.object_center_x = cx
+                self.object_center_y = cy
+                # Draw a red rectangle around the phone
+                cv2.rectangle(img=image_objects, pt1=(int(box[0]), int(box[1])), pt2=(int(box[2]), int(box[3])),
+                              color=(255, 0, 0), thickness=2)
+
                 text = "{}:{:.2f}".format(objects.names[class_id], confidence)
                 cv2.putText(img=image_objects,
                             text=text,
@@ -49,31 +74,10 @@ class YOLOIntegration:
                     color=(0, 255, 0),
                     thickness=1)
         self.last_frame_timestamp = time.time()
+
         return cv2.cvtColor(src=image_objects, code=cv2.COLOR_RGB2BGR)
 
-    def draw_object_indicator(self, image, box):
-        cx = int((box[0] + box[2]) / 2)
-        cy = int((box[1] + box[3]) / 2)
 
-        if cx > (2 / 3) * image.shape[1] and (1 / 3) * image.shape[0] < cy < (2 / 3) * image.shape[0]:
-            cv2.rectangle(img=image, pt1=(image.shape[1] - 10, 0),
-                          pt2=(image.shape[1] - 10, image.shape[0]),
-                          color=(255, 255, 255), thickness=6)
-        elif cx < (1 / 3) * image.shape[1] and (1 / 3) * image.shape[0] < cy < (2 / 3) * image.shape[0]:
-            cv2.rectangle(img=image, pt1=(10, 0), pt2=(10, image.shape[0]),
-                          color=(255, 255, 255), thickness=6)
-        elif cy > (2 / 3) * image.shape[0] and (1 / 3) * image.shape[1] < cx < (2 / 3) * image.shape[1]:
-            cv2.rectangle(img=image, pt1=(0, image.shape[0] - 10),
-                          pt2=(image.shape[1], image.shape[0] - 10),
-                          color=(255, 255, 255), thickness=6)
-        elif cy < (1 / 3) * image.shape[0] and (1 / 3) * image.shape[1] < cx < (2 / 3) * image.shape[1]:
-            cv2.rectangle(img=image, pt1=(0, 10), pt2=(image.shape[1], 10),
-                          color=(255, 255, 255), thickness=6)
-        cv2.rectangle(img=image, pt1=(int(box[0]), int(box[1])), pt2=(int(box[2]), int(box[3])), color=(255, 0, 0),
-                      thickness=2)
-
-        self.object_center_x = cx
-        self.object_center_y = cy
 
     def get_object_position(self):
         return self.object_center_x, self.object_center_y
@@ -86,10 +90,14 @@ class YOLOIntegration:
         ret, image = self.cap.read()
     def update_camera(self):
         _, image = self.cap.read()
-        image = image[:, ::-1, :]
-        cv2.imshow("Image", image)
-        self.process_frame(image)
+        image = cv2.flip(image, 1)
 
-    def close_camera(self):
+        processed_frame = self.process_frame(image)
+
+        cv2.imshow(winname="Image", mat=processed_frame)
+
+
+def close_camera(self):
         self.cap.release()
         cv2.destroyAllWindows()
+
